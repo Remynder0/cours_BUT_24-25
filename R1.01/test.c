e0a975c7feff22fcef1ebdd8ccc6d8008bdee7ae
Remynder0 <remynderyt@gmail.com>
2024-09-08 21:47:46 +0200

    TP1 C

---
+++ R1.01/test.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main(void) {
+    int entier= 42;
+    double decimal= 5.1;
+    printf("Par exemple, %d est entier et %3f %lf %04.2f est decimal. \n", entier, decimal, decimal ,decimal);
+    return 0;
+}
---
+++ R1.01/tp1/exo1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+
+int au_carre(int x){
+	return x*x;
+}
+
+
+
+
+
+int main(void){
+	printf("Q1 : %d",au_carre(72));
+	return 0;
+}
